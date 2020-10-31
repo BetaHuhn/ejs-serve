8cce1bba4dd85984b4952147ca93473f2dd59537
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-10-31 12:52:57 +0100

    Use relative path to reload script

--- src/utils/reloadScript.js
+++ src/utils/reloadScript.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
+const path = require('path')
 
 const prepare = (port) => {
-	const raw = fs.readFileSync('./src/scripts/reload.js', 'utf8')
+	const raw = fs.readFileSync(path.join(__dirname, '../scripts/reload.js'), 'utf8')
 	return raw.replace('WS_ENDPOINT_CHANGE', port)
 }
 
